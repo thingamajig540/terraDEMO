8082443dfe39ac0564d3199faba34f39e9185263
thingamajig540 <150844577+thingamajig540@users.noreply.github.com>
2023-11-14 20:11:41 -0800

    Update main.tf

--- main.tf
+++ main.tf
@@ -31,7 +31,7 @@ data "aws_ami" "ubuntu" {
 
 resource "aws_instance" "web" {
   ami           = data.aws_ami.ubuntu.id
-  instance_type = "t3.micro"
+  instance_type = "t2.micro"
 
   tags = {
     Name = "HelloWorld"
