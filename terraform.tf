c0ece6507f36ea995e630ae6d66aee49ffd274f2
thingamajig540 <150844577+thingamajig540@users.noreply.github.com>
2023-12-01 15:45:53 -0800

    terraform.tf

--- terraform.tf
+++ terraform.tf
@@ -7,13 +7,3 @@ terraform {
       name = "learn-terraform-cloud"
     }
   }
-
-  required_providers {
-    aws = {
-      source  = "hashicorp/aws"
-      version = "~> 3.28.0"
-    }
-  }
-
-  required_version = ">= 0.14.0"
-}
